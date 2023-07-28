50568c10bd443c9fca73b6e8e9c7f74aba9d48c6
Kevin Everrett <keverrett1989@gmail.com>
2023-07-28 15:07:27 -0400

    Hero Section...

---
+++ components/CustomButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from 'next/image';
+
+export const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn`}
+      onClick={() => {}}
+    >
+      CustomButton
+    </button>
+  );
+};
